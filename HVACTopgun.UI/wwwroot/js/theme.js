3056d5041b3b2f1d937cb135a2eb80d0fdba0e85
Casey Spaulding <casey.spaulding@me.com>
2023-08-02 14:36:19 -0400

    updated blog template

--- HVACTopgun.UI/wwwroot/js/theme.js
+++ HVACTopgun.UI/wwwroot/js/theme.js
@@ -1,12 +1,34 @@
 ﻿window.openModal = (id) => document.getElementById(id).showModal();
 window.closeModal = (id) => document.getElementById(id).close();
-
+window.addEventListener('scroll', () => {
+    var scrollY = window.scrollY || document.documentElement.scrollTop;
+    DotNet.invokeMethodAsync('YourAssemblyName', 'HandleScrollEventFromJS', scrollY);
+});
 export function setTheme(theme) {
     if (theme === 'dark') {
         document.documentElement.classList.add('dark');
     } else if (theme === 'light') {
         document.documentElement.classList.remove('dark');
     }
+}
+
+export function bindScrollEvent() {
+    indow.addEventListener('scroll', () => {
+        const scrollPosition = window.scrollY || document.documentElement.scrollTop;
+        objRef.invokeMethodAsync('HandleScrollEventFromJS', scrollPosition);
+    });
+}
+
+function scrollEventArgsCreator(event) {
+    return {
+        scrollPosition: window.scrollY || document.documentElement.scrollTop
+    };
+}
+
+export function afterStarted(blazor) {
+    blazor.registerCustomEventType('onscrollcustom', {
+        createEventArgs: scrollEventArgsCreator
+    });
 }
 function scrollEventArgsCreator(event) {
     return {
@@ -16,7 +38,8 @@ function scrollEventArgsCreator(event) {
 function toggleBanner() {
     var scrollPosition = window.scrollY;
     var banner = document.querySelector('.bg-blue-700');
-    if (scrollPosition > 50) { // You can adjust this value
+    if (scrollPosition > 50) {
+        // You can adjust this value
         banner.style.display = 'none';
     } else {
         banner.style.display = 'block';
@@ -25,9 +48,9 @@ function toggleBanner() {
 
 window.scrollFunctions = {
     registerScroll: function (dotNetObject) {
-        window.addEventListener("scroll", function () {
+        window.addEventListener('scroll', function () {
             var scrollY = window.scrollY || document.documentElement.scrollTop;
-            dotNetObject.invokeMethodAsync("HandleScrollEvent", scrollY);
+            dotNetObject.invokeMethodAsync('HandleScrollEvent', scrollY);
         });
     }
 };
@@ -42,12 +65,18 @@ function toggleMobileMenu() {
 
 function loadscript() {
     var script = document.createElement('script');
-    script.src = '_content/Syncfusion.Blazor.Core/scripts/syncfusion-blazor.min.js';
+    script.src =
+        '_content/Syncfusion.Blazor.Core/scripts/syncfusion-blazor.min.js';
     script.type = 'text/javascript';
     document.body.appendChild(script);
 }
 
 window.toggleDrawer = () => {
-    const drawer = document.querySelector('.relative.bg-white.h-full.transform.transition-transform.duration-300.ease-in-out');
-    drawer.style.transform = drawer.style.transform === 'translateX(0)' ? 'translateX(-100%)' : 'translateX(0)';
-}
+    const drawer = document.querySelector(
+        '.relative.bg-white.h-full.transform.transition-transform.duration-300.ease-in-out'
+    );
+    drawer.style.transform =
+        drawer.style.transform === 'translateX(0)'
+            ? 'translateX(-100%)'
+            : 'translateX(0)';
+};
